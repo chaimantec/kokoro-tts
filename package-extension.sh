ce3b462a1c64687133deb5c95bb2215b0a9696a5
chaimantec <nantapon@chaimantec.com>
2025-05-19 02:02:42 +0700

    updated

--- package-extension.sh
+++ package-extension.sh
@@ -7,7 +7,7 @@ npm run build
 # Create a zip file of the extension
 echo "Packaging Kokoro TTS Engine extension..."
 cd dist
-zip -r ../kokoro-tts-engine.zip ./*
+zip -r ../kokoro-speak.zip ./*
 cd ..
-echo "Extension packaged as kokoro-tts-engine.zip"
+echo "Extension packaged as kokoro-speak.zip"
 echo "You can install this in Chrome by going to chrome://extensions/, enabling Developer mode, and dragging the zip file onto the page."
